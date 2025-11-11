524251cf7247b7eb02d12adeca455d197bbb34dd
TheeratanaKotipang <6631501055@lamduan.mfu.ac.th>
2025-11-11 18:07:24 +0700

    update sport.js ldashboard.js and server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import authRoutes from "./backend/routes/auth.js";
 import sportRoutes from "./backend/routes/sport.js";
+import dashboardRoutes from "./backend/routes/sport.js";
 
 const app = express();
 app.use(cors());
@@ -10,7 +11,8 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.use("/api/auth", authRoutes);
-app.use("/api/sport", sportRoutes)
+app.use("/api/sport", sportRoutes);
+app.use("/api/dashboard", dashboardRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
