227d684fcedf9e3cfad028e526f1a30c07a5f9eb
Pnt-CoMExE <pnt@comexe.com>
2026-05-24 20:36:07 +0700

    Fix test assertion to match actual welcome page content

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:project_mobile_app/main.dart';
 
 void main() {
   testWidgets('App launches smoke test', (WidgetTester tester) async {
     await tester.pumpWidget(const SportBorrowingApp());
-    expect(find.text('Welcome'), findsWidgets);
+    // Welcome page shows the app title on first launch
+    expect(find.text('SPORT EQUIPMENT'), findsWidgets);
   });
 }
