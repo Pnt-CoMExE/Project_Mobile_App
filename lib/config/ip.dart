8d364f1d64f192f0a8575b608aea0bb95429cec8
Pnt-CoMExE <pnt@comexe.com>
2026-05-24 21:01:27 +0700

    Update IP to 10.0.2.2 for Android emulator testing

--- lib/config/ip.dart
+++ lib/config/ip.dart
@@ -3,7 +3,7 @@
 // ถ้ามี api ใหม่ใช้ IP ใน dart ที่ไม่มีในนี้ สร้างใหม่ตรงนี้เท่านั้น
 
 /// เปลี่ยนแค่ IP ตรงนี้เวลาเปลี่ยนเครื่อง
-const String kServerIp = "192.168.1.186";
+const String kServerIp = "10.0.2.2";
 const String kServerPort = "3000";
 
 /// Base host เช่น http://10.10.0.25:3000
